230fb6e4f8f576f9229bb723f80ac24911631e23
Jonathon Volkmar <jonathonvolkmar@gmail.com>
2011-10-01 10:09:47 -0400

    test

--- web.js
+++ web.js
@@ -3,9 +3,13 @@ var nowjs = require("now");
 
 var app = express.createServer(express.logger());
 
+var everyone = nowjs.initialize(app);
+
 app.get('/', function(request, response) {
   response.send('Hello World!');
 });
+
+app.use(express.staticProvider(__dirname + '/public'));
 
 var port = process.env.PORT || 3000;
 app.listen(port, function() {
